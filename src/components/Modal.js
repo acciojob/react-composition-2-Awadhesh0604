8b8bb5fdfe33c15da14d7131324279b410008997
Awadhesh <awadheshmzp1997@gmail.com>
2023-07-21 12:51:49 +0530

    Done

---
+++ src/components/Modal.js
@@ -0,0 +1,27 @@
+
+const Modal = ({ show, onClose, children }) => {
+    const modalRef = useRef(null);
+  
+    const handleCloseModal = (event) => {
+      if (event.target === modalRef.current) {
+        onClose();
+      }
+    };
+  
+    if (!show) {
+      return null;
+    }
+  
+    return (
+      <div className="modal-overlay" onClick={handleCloseModal} ref={modalRef}>
+        <div className="modal-dialog" onClick={onClose}>
+          <button className="modal-close" onClick={onClose}>
+            Close
+          </button>
+          <div className="modal-content">{children}</div>
+        </div>
+      </div>
+    );
+  };
+  
+  export default Modal;
